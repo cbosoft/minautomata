0ae715f4c097316e1f492e66ad0cf16b3df2642e
Christopher Boyle <chrismjboyle@gmail.com>
2022-02-20 14:36:07 +0000

    Add some basic documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
-mod colour;
-mod game;
-mod kinds;
-mod actions;
-mod particle;
-mod background;
-mod salt;
-mod concrete;
-mod water;
-mod point;
-mod log;
+//! MINAUTOMATA - a "falling sand" style game written in rust for the web.
+//! 
+//! MINAUTOMATA is compiled to web assembly (wasm) using wasm-pack. This
+//! compiles the rust to wasm, and generates the javascript interface necessary
+//! for running in a browser.
+//! 
+//! The game is based on the idea of "cellular automata" - inspired by the 
+//! Noita developers [talk at GDC 2019](https://www.youtube.com/watch?v=prXuyMCgbTc).
+//! Each cell - or particle - has a set of rules that it must follow. For
+//! example: a particle of sand falls down if it can so one of its rules is to
+//! see if there's space directly below and if there is, move into it.
+//! 
+
+pub mod actions;
+pub mod colour;
+pub mod game;
+pub mod kinds;
+pub mod log;
+pub mod point;
+pub mod particles;
--- src/log.rs
+++ src/log.rs
@@ -2,6 +2,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
+    /// Bring the external function "console.log" into rust.
     #[wasm_bindgen(js_namespace = console)]
     pub fn log(s: String);
 }
